d1729a80a377f791fa2f22b409705a950f2c19df
EvertonCa <eve.023@hotmail.com>
2020-02-06 17:30:16 -0300

    Initial Commit

---
+++ Carnaval 4.0 Rosas-FEITests/Carnaval_4_0_Rosas_FEITests.swift
@@ -0,0 +1,34 @@
+//
+//  Carnaval_4_0_Rosas_FEITests.swift
+//  Carnaval 4.0 Rosas-FEITests
+//
+//  Created by Everton Cardoso on 06/02/20.
+//  Copyright © 2020 Everton Cardoso. All rights reserved.
+//
+
+import XCTest
+@testable import Carnaval_4_0_Rosas_FEI
+
+class Carnaval_4_0_Rosas_FEITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
